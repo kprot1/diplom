adbcae44a7b7bd1b30f0986fe70f26d2b00b9ecf
kprot <kirikpv12@yandex.ru>
2018-05-29 23:54:18 +0400

    Поправил дефолтный контроллер, всякие настройки окружения.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/test/", name="test")
+     * @param Request $request
+     * @return Response
+     * @throws \InvalidArgumentException
      */
-    public function indexAction(Request $request)
+    public function testAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return new Response('123');
     }
 }
