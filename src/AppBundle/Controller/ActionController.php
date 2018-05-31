02388f01fa81726b2feb5ccf9fc2210596ada464
kprot <kirikpv12@yandex.ru>
2018-05-31 23:21:15 +0400

    Немного настроил контроллер акций.

--- src/AppBundle/Controller/ActionController.php
+++ src/AppBundle/Controller/ActionController.php
@@ -2,12 +2,58 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Action;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class ActionController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * Отображение списка всех акций
+     *
+     * @Route(path="/action/list", name="action_list")
+     */
+    public function listAction()
     {
-        return $this->render('', array('name' => $name));
+        $actions = $this->getDoctrine()->getRepository(Action::class)->findAll();
+
+        return $this->render('action/list.html.twig', [
+            'actions' => $actions
+        ]);
+    }
+
+    /**
+     * @Route(path="/action/view/{id}", name="action_view")
+     *
+     * @param int $id
+     */
+    public function viewAction(int $id)
+    {
+        return new Response('123');
+    }
+
+    /**
+     * Создание новой акции
+     *
+     * @Route(path="/action/create", name="action_create")
+     *
+     * @param int $id
+     */
+    public function createAction(int $id)
+    {
+
+    }
+
+    /**
+     * Редактирование существующей акции
+     *
+     * @Route(path="/action/edit/{id}", name="action_edit")
+     *
+     * @param int $id
+     */
+    public function editAction(int $id)
+    {
+
     }
 }
